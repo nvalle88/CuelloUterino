58e38d0becff712837431bb1b245494fdf2219b5
Néstor Valle <nestorvalle880416@gmail.com>
2018-03-02 21:11:58 -0500

    Agregar archivos de proyecto.

---
+++ CuelloUterino/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CuelloUterino.Startup))]
+namespace CuelloUterino
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
