7c06d8b01e50df44c6fae40fa7f1f257c5190486
Néstor Valle <nestorvalle880416@gmail.com>
2018-03-15 15:50:10 -0500

    se agrega funcionalidad de buscar

--- CuelloUterino/Controllers/HomeController.cs
+++ CuelloUterino/Controllers/HomeController.cs
@@ -17,116 +17,168 @@ namespace CuelloUterino.Controllers
         [HttpPost]
         public ActionResult Index(AnaliticaViewModel analiticaViewModel)
         {
-            //var cantidadTotal = db.Informe.Where(x =>
-            //                                        (
-            //                                         x.FechaMuestra.Year >= analiticaViewModel.FechaInicio.Year
-            //                                         && x.FechaMuestra.Month >= analiticaViewModel.FechaInicio.Month
-            //                                         && x.FechaMuestra.Day >= analiticaViewModel.FechaInicio.Day
-            //                                        )
 
-            //                                        &&
+            var lista = db.Informe.Where(x=>x.FechaMuestra>=analiticaViewModel.FechaInicio && x.FechaMuestra<=analiticaViewModel.FechaFin
+                                       ).ToList();
 
-            //                                        (
-            //                                         x.FechaMuestra.Year <= analiticaViewModel.FechaFin.Year
-            //                                         && x.FechaMuestra.Month <= analiticaViewModel.FechaFin.Month
-            //                                         && x.FechaMuestra.Day <= analiticaViewModel.FechaFin.Day
-            //                                        )
-            //                                    )
-            //                                     .ToList().Count;
+            if (lista.Count != 0)
+            {
+                 lista = lista.OrderBy(x => x.FechaMuestra).ToList();
 
-            //if (cantidadTotal != 0)
-            //{
+                var fechaInicio = lista.FirstOrDefault().FechaMuestra;
+                var fechaFin = lista.LastOrDefault().FechaMuestra;
 
 
-            //    var positivo = db.Informe.Where(x => x.ResultadoPruebaHibrida.Descripcion == Constantes.Positivo &&
-            //                                           (
-            //                                             x.FechaMuestra.Year >= analiticaViewModel.FechaInicio.Year
-            //                                             && x.FechaMuestra.Month >= analiticaViewModel.FechaInicio.Month
-            //                                             && x.FechaMuestra.Day >= analiticaViewModel.FechaInicio.Day
-            //                                            )
+                double HPVTotal = lista.Where(x => x.ResultadoPruebaHibrida.Descripcion == Constantes.Positivo).ToList().Count;
 
-            //                                            &&
+                double NegativoParaMalignidadTotal = (lista.Where(x => x.IdDiagnosticoCitologico == 1).ToList().Count());
+                double InflamacionleveTotal = (lista.Where(x => x.IdDiagnosticoCitologico == 2).ToList().Count());
+                double InflamacionModeradaTotal = (lista.Where(x => x.IdDiagnosticoCitologico == 3).ToList().Count());
+                double InflamacionSeveraTotal = (lista.Where(x => x.IdDiagnosticoCitologico == 4).ToList().Count());
+                double ASCUSTotal = (lista.Where(x => x.IdDiagnosticoCitologico == 5).ToList().Count());
+                double AGUSTotal = (lista.Where(x => x.IdDiagnosticoCitologico == 6).ToList().Count());
+                double LIEBGTotal = (lista.Where(x => x.IdDiagnosticoCitologico == 7).ToList().Count());
+                double LIEAGTotal = (lista.Where(x => x.IdDiagnosticoCitologico == 8).ToList().Count());
+                double CarcinomaInvasorTotal = (lista.Where(x => x.IdDiagnosticoCitologico == 9).ToList().Count());
+                double AdenocarcinomaTotal = (lista.Where(x => x.IdDiagnosticoCitologico == 10).ToList().Count());
+                double ASCHTotal = (lista.Where(x => x.IdDiagnosticoCitologico == 12).ToList().Count());
+                double OtrosTotal = (lista.Where(x => x.IdDiagnosticoCitologico == 13).ToList().Count());
 
-            //                                            (
-            //                                             x.FechaMuestra.Year <= analiticaViewModel.FechaFin.Year
-            //                                             && x.FechaMuestra.Month <= analiticaViewModel.FechaFin.Month
-            //                                             && x.FechaMuestra.Day <= analiticaViewModel.FechaFin.Day
-            //                                            )
-            //                                            ).ToList().Count;
 
 
-            //    var negativo = db.Informe.Where(x => x.ResultadoPruebaHibrida.Descripcion == Constantes.Negativo &&
-            //                                    (
-            //                                                 x.FechaMuestra.Year >= analiticaViewModel.FechaInicio.Year
-            //                                                 && x.FechaMuestra.Month >= analiticaViewModel.FechaInicio.Month
-            //                                                 && x.FechaMuestra.Day >= analiticaViewModel.FechaInicio.Day
-            //                                                )
+                double NegativoParaMalignidad = (NegativoParaMalignidadTotal * 100) / HPVTotal;
+                double Inflamacionleve = (InflamacionleveTotal * 100) / HPVTotal;
+                double InflamacionModerada = (InflamacionModeradaTotal * 100) / HPVTotal;
+                double InflamacionSevera = (InflamacionSeveraTotal * 100) / HPVTotal;
+                double ASCUS = (ASCUSTotal * 100) / HPVTotal;
+                double AGUS = (AGUSTotal * 100) / HPVTotal;
+                double LIEBG = (LIEBGTotal * 100) / HPVTotal;
+                double LIEAG = (LIEAGTotal * 100) / HPVTotal;
+                double CarcinomaInvasor = (CarcinomaInvasorTotal * 100) / HPVTotal;
+                double Adenocarcinoma = (AdenocarcinomaTotal * 100) / HPVTotal;
+                double ASCH = (ASCHTotal * 100) / HPVTotal;
+                double Otros = (OtrosTotal * 100) / HPVTotal;
 
-            //                                                &&
 
-            //                                                (
-            //                                                 x.FechaMuestra.Year <= analiticaViewModel.FechaFin.Year
-            //                                                 && x.FechaMuestra.Month <= analiticaViewModel.FechaFin.Month
-            //                                                 && x.FechaMuestra.Day <= analiticaViewModel.FechaFin.Day
-            //                                                )
-            //                                                ).ToList().Count;
 
-            //    var indeterminado = db.Informe.Where(x => x.ResultadoPruebaHibrida.Descripcion == Constantes.Indeterminado &&
-            //                                           (
-            //                                             x.FechaMuestra.Year >= analiticaViewModel.FechaInicio.Year
-            //                                             && x.FechaMuestra.Month >= analiticaViewModel.FechaInicio.Month
-            //                                             && x.FechaMuestra.Day >= analiticaViewModel.FechaInicio.Day
-            //                                            )
 
-            //                                            &&
+                ////Tipo VPN/ Histológico
 
-            //                                            (
-            //                                             x.FechaMuestra.Year <= analiticaViewModel.FechaFin.Year
-            //                                             && x.FechaMuestra.Month <= analiticaViewModel.FechaFin.Month
-            //                                             && x.FechaMuestra.Day <= analiticaViewModel.FechaFin.Day
-            //                                            )
-            //                                            ).ToList().Count;
+                var SinRealizar = (lista.Where(x => x.IdTipoHistologico == 0).ToList().Count() * 100) / Convert.ToInt32(HPVTotal);
+                var CervicitisAguda = (lista.Where(x => x.IdDiagnosticoCitologico == 1).ToList().Count() * 100) / Convert.ToInt32(HPVTotal);
+                var CervicitisCronica = (lista.Where(x => x.IdDiagnosticoCitologico == 2).ToList().Count() * 100) / Convert.ToInt32(HPVTotal);
+                var AtipiaCoilociticaSinDisplasia = (lista.Where(x => x.IdDiagnosticoCitologico == 3).ToList().Count() * 100) / Convert.ToInt32(HPVTotal);
+                var NICI = (lista.Where(x => x.IdDiagnosticoCitologico == 4).ToList().Count() * 100) / Convert.ToInt32(HPVTotal);
+                var NICII = (lista.Where(x => x.IdDiagnosticoCitologico == 5).ToList().Count() * 100) / Convert.ToInt32(HPVTotal);
+                var NICIII = (lista.Where(x => x.IdDiagnosticoCitologico == 6).ToList().Count() * 100) / Convert.ToInt32(HPVTotal);
+                var CIS = (lista.Where(x => x.IdDiagnosticoCitologico == 7).ToList().Count() * 100) / Convert.ToInt32(HPVTotal);
+                var CarcinomaEscamoso = (lista.Where(x => x.IdDiagnosticoCitologico == 8).ToList().Count() * 100) / Convert.ToInt32(HPVTotal);
+                var DisplasiaGlandularEndocervical = (lista.Where(x => x.IdDiagnosticoCitologico == 9).ToList().Count() * 100) / Convert.ToInt32(HPVTotal);
+                var AdenocarcinomaTipo = (lista.Where(x => x.IdDiagnosticoCitologico == 10).ToList().Count() * 100) / Convert.ToInt32(HPVTotal);
+                var OtrosTipo = (lista.Where(x => x.IdDiagnosticoCitologico == 11).ToList().Count() * 100) / Convert.ToInt32(HPVTotal);
 
-            //    var adecuada = db.Informe.Where(x => x.Valoracion.Descripcion == Constantes.Adecuadas &&
-            //                                           (
-            //                                             x.FechaMuestra.Year >= analiticaViewModel.FechaInicio.Year
-            //                                             && x.FechaMuestra.Month >= analiticaViewModel.FechaInicio.Month
-            //                                             && x.FechaMuestra.Day >= analiticaViewModel.FechaInicio.Day
-            //                                            )
 
-            //                                            &&
 
-            //                                            (
-            //                                             x.FechaMuestra.Year <= analiticaViewModel.FechaFin.Year
-            //                                             && x.FechaMuestra.Month <= analiticaViewModel.FechaFin.Month
-            //                                             && x.FechaMuestra.Day <= analiticaViewModel.FechaFin.Day
-            //                                            )
-            //                                            ).ToList().Count;
+                //
 
-            //    var porcientoPositivo = (positivo * 100) / cantidadTotal;
+                double CitologiasTotales = lista.Where(x => x.CitologiaVPH == true).ToList().Count();
 
-            //    var porcientoNegativo = (negativo * 100) / cantidadTotal;
+                double NegativoParaMalignidadCitologias = 0;
+                double InflamacionleveCitologias = 0;
+                double InflamacionModeradaCitologias = 0;
+                double InflamacionSeveraCitologias = 0;
+                double ASCUSCitologias = 0;
+                double AGUSCitologias = 0;
+                double LIEBGCitologias = 0;
+                double LIEAGCitologias = 0;
+                double CarcinomaInvasorCitologias = 0;
+                double AdenocarcinomaCitologias = 0;
+                double ASCHCitologias = 0;
+                double OtrosCitologias = 0;
 
-            //    var porcientoIndeterminado = (indeterminado * 100) / cantidadTotal;
+                if (CitologiasTotales != 0)
+                {
+                    double a = NegativoParaMalignidadTotal / CitologiasTotales * 100;
+                    NegativoParaMalignidadCitologias = (NegativoParaMalignidadTotal / CitologiasTotales) * 100;
+                    InflamacionleveCitologias = (InflamacionleveTotal / CitologiasTotales) * 100;
+                    InflamacionModeradaCitologias = (InflamacionModeradaTotal / CitologiasTotales) * 100;
+                    InflamacionSeveraCitologias = (InflamacionSeveraTotal / CitologiasTotales) * 100;
+                    ASCUSCitologias = (ASCUSTotal / CitologiasTotales) * 100;
+                    AGUSCitologias = (AGUSTotal / CitologiasTotales) * 100;
+                    LIEBGCitologias = (LIEBGTotal / CitologiasTotales) * 100;
+                    LIEAGCitologias = (LIEAGTotal / CitologiasTotales) * 100;
+                    CarcinomaInvasorCitologias = (CarcinomaInvasorTotal / CitologiasTotales) * 100;
+                    AdenocarcinomaCitologias = (AdenocarcinomaTotal / CitologiasTotales) * 100;
+                    ASCHCitologias = (ASCHTotal / CitologiasTotales) * 100;
+                    OtrosCitologias = (OtrosTotal / CitologiasTotales) * 100;
+                }
 
-            //    var porcientoAdecuado = (adecuada * 100) / cantidadTotal;
+                var viewModel = new AnaliticaViewModel
+                {
+                    NegativoParaMalignidad = NegativoParaMalignidad,
+                    Adenocarcinoma = Adenocarcinoma,
+                    AGUS = AGUS,
+                    ASCH = ASCH,
+                    ASCUS = ASCUS,
+                    CarcinomaInvasor = CarcinomaInvasor,
+                    Inflamacionleve = Inflamacionleve,
+                    InflamacionModerada = InflamacionModerada,
+                    InflamacionSevera = InflamacionSevera,
+                    LIEAG = LIEAG,
+                    LIEBG = LIEBG,
+                    Otros = Otros,
 
-            //    var viewModel = new AnaliticaViewModel
-            //    {
-            //        PorcientoAdecuadas = Convert.ToInt32(porcientoAdecuado),
-            //        PorcientoIndeterminado = Convert.ToInt32(porcientoIndeterminado),
-            //        PorcientoNegativo = Convert.ToInt32(porcientoNegativo),
-            //        PorcientoPositivo = Convert.ToInt32(porcientoPositivo),
-            //        FechaFin = analiticaViewModel.FechaFin,
-            //        FechaInicio = analiticaViewModel.FechaInicio,
 
-            //        Negativo = negativo,
-            //        Positivo = positivo,
-            //        Indeterminado = indeterminado,
-            //        Adecuado = adecuada,
-            //        Total = cantidadTotal,
-            //    };
-                return View(new AnaliticaViewModel());
+                    ////Tipo VPN/ Histológico
+
+                    AdenocarcinomaTipo = AdenocarcinomaTipo,
+                    AtipiaCoilociticaSinDisplasia = AtipiaCoilociticaSinDisplasia,
+                    CarcinomaEscamoso = CarcinomaEscamoso,
+                    CervicitisAguda = CervicitisAguda,
+                    CervicitisCronica = CervicitisCronica,
+                    CIS = CIS,
+                    DisplasiaGlandularEndocervical = DisplasiaGlandularEndocervical,
+                    NICI = NICI,
+                    NICII = NICII,
+                    NICIII = NICIII,
+                    OtrosTipo = OtrosTipo,
+                    SinRealizar = SinRealizar,
+
+                    ////
+
+
+                    AdenocarcinomaCitologias = AdenocarcinomaCitologias,
+                    AGUSCitologias = AGUSCitologias,
+                    ASCHCitologias = ASCHCitologias,
+                    ASCUSCitologias = ASCUSCitologias,
+                    CarcinomaInvasorCitologias = CarcinomaInvasorCitologias,
+                    InflamacionleveCitologias = InflamacionleveCitologias,
+                    InflamacionModeradaCitologias = InflamacionModeradaCitologias,
+                    InflamacionSeveraCitologias = InflamacionSeveraCitologias,
+                    LIEAGCitologias = LIEAGCitologias,
+                    LIEBGCitologias = LIEBGCitologias,
+                    NegativoParaMalignidadCitologias = NegativoParaMalignidadCitologias,
+                    OtrosCitologias = OtrosCitologias,
+
+                    FechaInicio = fechaInicio,
+                    FechaFin = fechaFin,
+
+
+                };
+
+                return View(viewModel);
+            }
+            else
+            {
+
+                var viewModel = new AnaliticaViewModel();
+                viewModel.FechaFin = analiticaViewModel.FechaFin;
+                viewModel.FechaInicio = analiticaViewModel.FechaInicio;
+                return View(viewModel);
+
+
+            }
+           
             //};
             //return View();
         }
